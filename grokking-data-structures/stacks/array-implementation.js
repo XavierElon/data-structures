27cec2403a869c3ae39b644c2927942cb4f3b3d1
Xavier <xavierh93@hotmail.com>
2023-11-04 09:21:04 -0400

    stack array-implementation

--- grokking-data-structures/stacks/array-implementation.js
+++ grokking-data-structures/stacks/array-implementation.js
@@ -10,4 +10,31 @@ class Stack {
     }
     this.stack[++this.top] = data
   }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty')
+    }
+    const data = this.stack[this.top]
+    this.stack[this.top--] = null
+    return data
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty')
+    }
+    return this.stack[this.top]
+  }
+
+  isEmpty() {
+    return this.top === -1
+  }
 }
+
+const stack = new Stack(3)
+stack.push(1)
+stack.push(5)
+stack.push(10)
+console.log(stack.peek())
+stack.push(20)
